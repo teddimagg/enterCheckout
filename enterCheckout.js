2d8a76ee3060a256ada25b35cce360b56d3d5e8c
teddimagg <teddi@menn.is>
2017-07-17 13:14:11 +0000

    commenting with jsDoc syntax

--- enterCheckout.js
+++ enterCheckout.js
@@ -1,10 +1,16 @@
 class EnterCheckout{
-	//ENTER INITIATION
+	/**
+	* Initates a new Enter instance from artist Id.
+	* @param {string} the Id of the artist for Enter.is
+	*/
 	constructor(artistId) {
 		this.artist = artistId;
 		this.cart = JSON.parse(localStorage.getItem('enterCart')) || [];
 	}
 
+	/**
+	* Gathers product data from Enter.is
+	*/
 	init() {
 		return new Promise((resolve, reject) => {
 			fetch('http://localhost:3000/api/artist/' + this.artist, {
@@ -21,20 +27,31 @@ class EnterCheckout{
 		});
 	}
 
-	//PRODUCT FUNCTIONALITY
+	/**
+	* This function returns a product from it's Id.
+	* @param {string} the Id of the product
+	* @returns {number} a product found from the input id
+	*/
 	productFromId(id){
 		return this.products.find((product) => {
 			return product._id === id;
 		});
 	}
 
+	/**
+	* This function sorts the products item.
+	* @param {string} field name to sort by.
+	* @param {string} [input="asc"] which order to sort by
+	*/
 	sortProducts(field, order){
 		this.products.sort((a, b) => {
 			return (order === 'desc') ? a[field] < b[field] : a[field] > b[field];
 		});
 	}
 
-	//CART FUNCTIONALITY
+	/**
+	* This function moves the user to a secure checkout link on Enter.is
+	*/
 	checkout() {
 		let props = ['id', 'size', 'color', 'quantity'];
 		let propCount = { id: false, size: false, color: false, quantity: false };
@@ -59,12 +76,20 @@ class EnterCheckout{
 		this.clearCart();
 	}
 
+	/**
+	* This function calculates the total price of the cart.
+	* @returns {number} the total price of the cart.
+	*/
 	cartTotal() {
 		return (this.cart.length) ? this.cart.map((product) => {
 			return Number(product.price) * Number(product.quantity);
 		}).reduce((x, y) => x + y) : 0;
 	}
 
+	/**
+	* This function adds a product to cart
+	* @param {Object} the product which is to be added to cart.
+	*/
 	addToCart(product) {
 		let contains = false;
 		this.cart.length && this.cart.forEach((p, index) => {
@@ -77,17 +102,27 @@ class EnterCheckout{
 		this._storeCart();
 	}
 
+	/**
+	* Manually updates cart object and the cart in local storage
+	* @param {[Object]} the new cart (array) of products to replace the current cart.
+	*/
 	updateCart(cart) {
 		this.cart = cart;
 		this._storeCart();
 	}
 
+	/**
+	* Completely clears the cart and updates local storage
+	*/
 	clearCart() {
 		this.cart = [];
 		this._storeCart();
 	}
 
 	// _ framework helpers
+	/**
+	* Stores the cart in local storage for later use
+	*/
 	_storeCart() {
 		localStorage.setItem('enterCart', JSON.stringify(this.cart));
 	}
